5b8a6887b7084b5f7ff35abd3ec8aa3317dc8047
Marshall Mattingly <marshall.mattingly@gmail.com>
2016-02-18 11:07:18 -0600

    updated

--- hw2/genome.cxx
+++ hw2/genome.cxx
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <mpi.h>
@@ -5,6 +6,7 @@
 #include <vector>
 
 #define READ_LEN    40
+#define CHR_LEN     51
 
 #define ARG_NONE    0
 #define ARG_CHR     1
@@ -26,35 +28,32 @@ bool parse_args( int argc, char **argv,
     int current = ARG_NONE;
     int complete = ARG_NONE;
 
-    while (argc--) {
+    for ( ; argc > 0; --argc, ++argv) {
         // first check for a new type
-        if (strcmp(*argv, "--chr")) {
+        if (strcmp(*argv, "--chr") == 0) {
             complete |= current;
             current = ARG_CHR;
             continue;
-        } else if (strcmp(*argv, "--read")) {
+        } else if (strcmp(*argv, "--read") == 0) {
             complete |= current;
             current = ARG_READ;
             continue;
         }
 
-        switch (current) {
-        case ARG_NONE:
-            cout << "Unknown argument: " << *argv << endl;
+        string s(*argv);
+        if (current == ARG_NONE) {
+            cerr << "Unknown argument: " << *argv << endl;
             chr.clear();
             read.clear();
             return false;
-        
-        case ARG_CHR:
-            chr.push_back(new std::string(*argv));
-            break;
-
-        case ARG_READ:
-            read.push_back(new std::string(*argv));
-            break;
-
-        default:
-            cout << "Unknown parsing error" << endl;
+        } else if (current == ARG_CHR) {
+            cout << "CHR  += '" << s << "'" << endl;
+            chr.push_back(s);
+        } else if (current == ARG_READ) {
+            cout << "READ += '" << s << "'" << endl;
+            read.push_back(s);
+        } else {
+            cerr << "Unknown parsing error" << endl;
             chr.clear();
             read.clear();
             return false;
@@ -62,21 +61,33 @@ bool parse_args( int argc, char **argv,
     }
 
     complete |= current;
-    return complete & ARG_CHR & ARG_READ;
+    return (complete & (ARG_CHR | ARG_READ)) == (ARG_CHR | ARG_READ);
 }
 
 bool isValid(char c)
 {
-    return c == 'N' || c == 'A' || c == 'C' || c == 'G' || c == 'T';
+    return  c == 'N' || c == 'A' || c == 'C' || c == 'G' || c == 'T' ||
+            c == 'n' || c == 'a' || c == 'c' || c == 'g' || c == 't';
 }
 
 bool nCount(char *s, int n)
 {
     int n_count = 0;
     for (int i = 0; i < n; ++i)
-        if (s[i] == 'N') n_count++;
+        if (s[i] == 'N' || s[i] == 'n') n_count++;
 
     return n_count;
+}
+
+void print_usage()
+{
+    cout << "Usage: ./genome N_READS N_CHR --chr CHR_FILE [CHR_FILE ...] --read READ_FILE [READ_FILE ...]" << endl;
+}
+
+int kill()
+{
+    MPI_Abort(MPI_COMM_WORLD, 1);
+    return 1;
 }
 
 int main(int argc, char **argv)
@@ -96,12 +107,26 @@ int main(int argc, char **argv)
         vector<string> chr_files;
         vector<string> read_files;
 
-        if (!parse_args(argc, argv, chr_files, read_files)) {
-            cout << "Usage: ./genome --chr CHR_FILE [CHR_FILE ...] --read READ_FILE [READ_FILE ...]" << endl;
+        // consume the first arg
+        --argc; ++argv;
 
-            MPI_Abort(MPI_COMM_WORLD, 1);
-            return 1;
+        // read in the n-max for reads and chr
+        if ((n_reads = strtol(*argv++, NULL, 10)) < 0) {
+            print_usage();
+            return kill();
         }
+        if ((n_chr = strtol(*argv++, NULL, 10)) < 0) {
+            print_usage();
+            return kill();
+        }
+
+        // parse our file args
+        if (!parse_args(argc-2, argv, chr_files, read_files)) {
+            print_usage();
+            return kill();
+        }
+
+        cout << "Read files: " << read_files.size() << endl;
 
         // read in our read files
         vector<char> reads;
@@ -110,9 +135,10 @@ int main(int argc, char **argv)
         for (auto it = read_files.begin(); it != read_files.end(); ++it) {
             // open the file
             string filename = *it;
+            cout << "Opening file: " << filename << endl;
             fs.open(filename, fstream::in);
             if (!fs.is_open()) {
-                cout << "Failed to open file: " << filename;
+                cerr << "Failed to open file: " << filename;
                 MPI_Abort(MPI_COMM_WORLD, 1);
                 return 1;
             }
@@ -135,6 +161,24 @@ int main(int argc, char **argv)
 
         // print out our reads
         cout << "Reads length: " << reads.size() << endl;
+    }
+
+    if (rank == 0) {
+        // go through the chr files
+        for (auto it = chr_files.begin(); it != chr_files.end(); ++it) {
+            string filename = *it;
+            char buf[CHR_LEN];
+
+            // skip the first line
+            fs.getline(buf, CHR_LEN);
+            if (!fs.good()) continue;
+
+            // read in each line
+            while (1) {
+                fs.getline(buf, CHR_LEN);
+                if (!fs.good()) break;
+            }
+        }
     }
 
     MPI_Finalize();
